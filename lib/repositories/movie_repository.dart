99f8e52a646caf7660c719af6570c2c5dfdd2f75
Dieison  Henrique Nonnenmacher <dieison.nonnenmacher@MBL-GP04V4GQ3P.local>
2025-05-09 11:29:17 -0300

    Adicionando persistência

--- lib/repositories/movie_repository.dart
+++ lib/repositories/movie_repository.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:movies_flix/app_config.dart';
 import 'package:movies_flix/models/movie.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MovieRepository {
   final Dio client = Dio(
@@ -14,13 +18,40 @@ class MovieRepository {
     ),
   );
 
+  SharedPreferences? _preferences;
+  final String moviesKey = 'movies';
+
+  FutureOr<SharedPreferences> get db async {
+    _preferences ??= await SharedPreferences.getInstance();
+    return _preferences!;
+  }
+
   Future<List<Movie>> getMovies() async {
     final response = await client.get(
       '/discover/movie',
       queryParameters: {'page': 1},
     );
-    return (response.data['results'] as List)
-        .map((movie) => Movie.fromJson(movie))
-        .toList();
+
+    final movies =
+        (response.data['results'] as List)
+            .map((movie) => Movie.fromJson(movie))
+            .toList();
+    final storage = await db;
+    storage.setStringList(
+      moviesKey,
+      movies.map((movie) => movie.toJson).toList(),
+    );
+    return movies;
+  }
+
+  Future<List<Movie>> getMoviesDb() async {
+    final storage = await db;
+
+    final movies = storage.getStringList(moviesKey);
+
+    if (movies == null || movies.isEmpty) {
+      return [];
+    }
+    return movies.map((movie) => Movie.fromJson(jsonDecode(movie))).toList();
   }
 }
